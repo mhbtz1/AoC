188242b533a6605dde50e19eecd2eb8e9b5a52d0
Matthew Habtezgi <mhbtz@mit.edu>
2024-12-26 19:25:24 -0500

    p2 part 1

--- p2/src/main.rs
+++ p2/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 fn p1(input_str: &str) -> i64 {
     let binding = fs::read_to_string(input_str).expect("ERROR: Could not read file!");
     // recall: str denotes an immutable sequence of UTF-8 bytes, String is a pointer to a heap-allocated sequence of UTF-8 bytes (i.e. can be dynamically grown)
@@ -5,12 +7,16 @@ fn p1(input_str: &str) -> i64 {
     let mut ans = 0;
 
     for content in contents {
-        let arr = contents.split(" ").collect::<Vec<&str>>();
-        let cur = arr[1];
-        let relation = if (arr[0] < cur) { 1 } else {0 };
-        let valid = true;
+        let arr = content.split(" ").collect::<Vec<_>>().into_iter().map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        println!("arr: {:?}", arr);
+        let mut cur = arr[1];
+        let relation : bool = match arr[0] < cur { 
+                                true => true,
+                                false => false      
+                              };
+        let mut valid = (cur - arr[0]).abs() >= 1 && (cur - arr[0]).abs() <= 3;
         for i in 2..arr.len() {
-            if (relation && cur < arr[i]) || (!relation && cur > arr[i]) {
+            if ((relation && cur < arr[i]) || (!relation && cur >= arr[i])) && ((cur - arr[i]).abs() >= 1 && (cur - arr[i]).abs() <= 3) {
                 cur = arr[i];
             } else {
                 valid = false;
@@ -30,7 +36,7 @@ fn p1(input_str: &str) -> i64 {
 fn main() {
     let part = std::env::args().nth(1).expect("Did not receive command line argument for part!");
     match part.as_str() {
-        "1" => {let ans = p1("src/input.txt"); println!("part 1: {}", ans);},
-        "2" => {let ans = p2("src/input.txt"); println!("part 2: {}", ans);}
+        "1" => {let ans = p1("src/input.txt"); println!("part 1: {}", ans);}
+        _ => { println!("Invalid part!"); }
     }
 }
