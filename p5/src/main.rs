f3e4c3f9f82172562650a250231f6de2daae33c4
Matthew Habtezgi <mhbtz@mit.edu>
2025-01-02 11:24:52 -0500

    p5

--- p5/src/main.rs
+++ p5/src/main.rs
@@ -1,154 +1,68 @@
+use std::{cmp::Ordering, collections::HashSet};
 
-use std::{collections::{HashSet, HashMap}, fs};
-use std::vec::Vec;
-use std::str::FromStr;
+#[allow(dead_code)]
+pub fn part1(input: &str) -> usize {
+    let (orderings, updates) = input.split_once("\n\n").unwrap();
 
-pub struct Solution;
+    let orderings: HashSet<(usize, usize)> = orderings
+        .lines()
+        .map(|line| (line[0..2].parse().unwrap(), line[3..].parse().unwrap()))
+        .collect();
 
-type Node = u64;
-pub type Graph = HashMap<Node, Vec<Node>>;
-static default: Vec<Node> = Vec::new();
+    let compare = |x: &usize, y: &usize| !orderings.contains(&(*y, *x));
 
-impl<'a> Solution {
+    let mut count = 0;
+    for update in updates.lines() {
+        let update: Vec<usize> = update.split(',').map(|x| x.parse().unwrap()).collect();
 
-    pub fn kahn(g: &'a Graph, ordering: &'a Vec<Node>, start: Option<&'a Node>) -> (bool, Vec<&'a Node>) {
-        let mut queue = Vec::new();
-        queue.push((ordering[0], 0) );
-        let mut vis = HashSet::new();
-        let mut valid = false;
-
-        while queue.len() > 0 {
-            let front = queue.pop().unwrap();
-            let nbrs: &Vec<Node> = &g.get(&front.0).unwrap_or(&default);
-            vis.insert(front);
-            if front.1 == ordering.len() - 1 {
-                valid = true;
-            }
-            for nbr in nbrs {
-                if vis.contains(&(*nbr, front.1+1) ) {
-                    continue;
-                }
-                if front.1 + 1 < ordering.len() && ordering[front.1 + 1] == *nbr {
-                    queue.push( (*nbr, front.1 + 1) );
-                }
-            }
+        if update.is_sorted_by(compare) {
+            count += update[update.len() / 2];
         }
-    
-        let mut reorder_nodes: Vec<&Node> = Vec::new();
-        let mut vis = HashSet::new();
-        if start.is_some() {
-            let mut second_queue: Vec<(&'a Node, Vec<&Node>)> = Vec::new();
-            second_queue.push( (start.unwrap(), vec![start.unwrap()]) ); 
-            reorder_nodes.push(start.unwrap());
-        
-            while second_queue.len() > 0 {
-                let front = second_queue.pop().unwrap();
-                vis.insert(front.clone());
-                //println!("current node: {:?}, current path: {:?}", front.0, &front.1);
-                if front.1.len() == ordering.len() {
-                    reorder_nodes = front.1.clone();
-                    break;
-                }
-
-                let nbrs: &Vec<Node> = &g.get(front.0).unwrap_or(&default);
-                
-                for nbr in nbrs { // nbr is type &Node
-                    let mut prev = front.1.clone();
-                    prev.push(nbr);
-                    if !vis.contains(&(nbr, prev.clone()) ) && (g[&front.1[front.1.len()-1]].iter().any(|item| std::ptr::eq(item, nbr))) && ordering.contains(nbr) {
-                        let mut prev = front.1.clone();
-                        prev.push(nbr);
-                        second_queue.push( (nbr, prev.clone()) );
-                    } else {
-                        //println!("result: {}", g[&front.1[front.1.len()-1]].iter().any(|item| std::ptr::eq(item, nbr)));
-                    }
-                }
-            }
-        }
-
-        return (valid, reorder_nodes);
     }
 
+    count
+}
 
-    pub fn p1(fpath: &str) -> u64 {
-        //println!("file path: {}", fpath); // testing ownership semantics
-        let fcontent = fs::read_to_string(fpath).expect("not able to read file!");
-        let vec = fcontent.split("\n\n").collect::<Vec<_>>();
-        
-        let mut ordering = vec[0].split('\n').into_iter().collect::<Vec<&str>>();
-        let mut g: Graph = HashMap::new();
-        let mut nodes: HashSet<u64> = HashSet::new();
-        
-        let mut start_node: u64 = 12;
-        for order in ordering {
-            let left = u64::from_str(&order[0..order.find("|").unwrap() ]).unwrap();
-            let right = u64::from_str(&order[order.find("|").unwrap()+1..]).unwrap();
-            g.entry(left).or_insert_with(Vec::new).push(right);
-            nodes.insert(left);
-            nodes.insert(right);
+#[allow(dead_code)]
+pub fn part2(input: &str) -> usize {
+    let (orderings, updates) = input.split_once("\n\n").unwrap();
+
+    let orderings: HashSet<(usize, usize)> = orderings
+        .lines()
+        .map(|line| (line[0..2].parse().unwrap(), line[3..].parse().unwrap()))
+        .collect();
+
+    let compare = |x: &usize, y: &usize| {
+        let (x, y) = (*x, *y);
+        if orderings.contains(&(x, y)) {
+            Ordering::Less
+        } else if orderings.contains(&(y, x)) {
+            Ordering::Greater
+        } else {
+            Ordering::Equal
         }
+    };
 
-        //println!("number of nodes in graph: {}", g.keys().len());
-        let updates = vec[1].split("\n").into_iter().collect::<Vec<&str>>();
-        let mut ans = 0;
-        for update in &updates {
-            let mut ordering = update.split(",").collect::<Vec<&str>>().into_iter().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-            let (valid, _) = Solution::kahn(&g, &mut ordering, None);
-            if valid {
-                ans += ordering[ordering.len() / 2];
+    updates
+        .lines()
+        .map(|update| {
+            let mut update: Vec<usize> = update.split(',').map(|x| x.parse().unwrap()).collect();
+
+            if update.is_sorted_by(|a, b| compare(a, b) != Ordering::Greater) {
+                0
+            } else {
+                update.sort_by(compare);
+                update[update.len() / 2]
             }
-        }
-        //println!("part 1: {}", ans);
-        ans
-    }  
-
-
-    pub fn p2(fpath: &str) -> u64 {
-        //println!("file path: {}", fpath); // testing ownership semantics
-        let fcontent = fs::read_to_string(fpath).expect("not able to read file!");
-        let vec = fcontent.split("\n\n").collect::<Vec<_>>();
-        
-        let mut ordering = vec[0].split('\n').into_iter().collect::<Vec<&str>>();
-        let mut g: Graph = HashMap::new();
-        
-        for order in ordering {
-            let left = u64::from_str(&order[0..order.find("|").unwrap() ]).unwrap();
-            let right = u64::from_str(&order[order.find("|").unwrap()+1..]).unwrap();
-            g.entry(left).or_insert_with(Vec::new).push(right);
-        }
-
-
-        let updates = vec[1].split("\n").into_iter().collect::<Vec<&str>>();
-        let mut ans = 0;
-        for update in &updates {
-            let mut ordering = update.split(",").collect::<Vec<&str>>().into_iter().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-            let mut setcoll: HashSet<Node> = ordering.clone().into_iter().collect::<HashSet<_>>();
-
-            let cln = ordering.clone();
-            for start in &cln {
-                let (valid, reordered_nodes) = Solution::kahn(&g, &ordering, Some(start));
-                if valid {
-                    break;
-                } else {
-                    if reordered_nodes.len() == cln.len() {
-                        ans += reordered_nodes[reordered_nodes.len() / 2];
-                        //println!("reordered: {:?}", &reordered_nodes);
-                        break;
-                    } else {
-                        //println!("reordered len: {}, cln len: {}", reordered_nodes.len(), cln.len());
-                    }
-                }
-                setcoll = ordering.clone().into_iter().collect::<HashSet<_>>();
-            }
-        }
-        println!("part 2: {}", ans);
-        return ans;
-    }    
-
+        })
+        .sum()
 }
 
 
+
 fn main() {
-    //println!("Hello, world!");
-    Solution::p2("src/smol_input.txt");
+    let input = include_str!("input.txt");
+    println!("input: {}", input);
+    println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
